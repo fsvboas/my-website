e529eeaa50ef7a33dd577bf4a48050b0438d2b05
fsvboas <fsvboas.dev@gmail.com>
2025-02-14 09:36:06 -0300

    chore: initial setup

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex w-full min-h-screen items-center justify-center">
+      <h1>fsvboas</h1>
+    </div>
+  );
+}
---
+++ app/utils/class-names.ts
@@ -0,0 +1,4 @@
+type ClassNameType = string | boolean | undefined | null;
+
+export const classNames = (...classNames: ClassNameType[]): string =>
+  classNames.filter(Boolean).join(" ").replace(/\s\s+/g, " ").trim();
