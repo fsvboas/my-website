46fd4626aa9dff4fc03217c1ded91a52efd1999d
fsvboas <fsvboas.dev@gmail.com>
2025-02-25 18:17:50 -0300

    refactor: minor changes in switch-theme-button

--- app/components/switch-theme-button.tsx
+++ app/components/switch-theme-button.tsx
@@ -5,12 +5,12 @@ import { useTheme } from "next-themes";
 import { Button } from "./ui/button";
 import { Show } from "./utils/show";
 
-const SwitchTheme = () => {
+const SwitchThemeButton = () => {
   const { theme, setTheme } = useTheme();
 
   return (
     <Button
-      className="absolute right-5 top-5 border border-gray-300 dark:border-gray-600 rounded-[8px] hover:bg-black duration-300 hover:text-white dark:hover:bg-white dark:hover:text-black"
+      className="shadow-none absolute right-6 top-6 border border-gray-300 dark:border-gray-600 rounded-[8px] hover:bg-black duration-300 hover:text-white dark:hover:bg-white dark:hover:text-black"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
       <Show when={theme === "dark"} fallback={<Sun />}>
@@ -20,4 +20,4 @@ const SwitchTheme = () => {
   );
 };
 
-export default SwitchTheme;
+export default SwitchThemeButton;
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AboutMeSection from "./components/about-me-section";
 import EducationSection from "./components/education-section";
 import PresentationCard from "./components/presentation-card";
 import SkillsSection from "./components/skills-section";
-import SwitchTheme from "./components/switch-theme";
+import SwitchThemeButton from "./components/switch-theme-button";
 import { Column } from "./components/utils/column";
 import WorkExperiencesSection from "./components/work-experiences-section";
 
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className="flex w-full min-h-screen justify-center bg-[#f4f2ee] dark:bg-[#101010] p-6 md:p-16">
       <Column className="w-full max-w-screen-md space-y-10">
-        <SwitchTheme />
+        <SwitchThemeButton />
         <PresentationCard />
         <AboutMeSection />
         <WorkExperiencesSection />
