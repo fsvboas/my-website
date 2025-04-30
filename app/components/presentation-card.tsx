a343ca529e0f26fd61ccbd6baa5868fa30fc21cb
fsvboas <fsvboas.dev@gmail.com>
2025-04-30 14:27:13 -0300

    refactor: improve presentation-card responsiveness

--- app/components/presentation-card.tsx
+++ app/components/presentation-card.tsx
@@ -9,7 +9,7 @@ const PresentationCard = () => {
   return (
     <Row className="items-center justify-between border border-gray-300 dark:border-gray-600 p-4 rounded-xl duration-300">
       <Column className="space-y-2">
-        <h1 className="text-2xl xs:text-3xl font-bold text-black dark:text-white">
+        <h1 className="text-2xl sm:text-3xl font-bold text-black dark:text-white">
           Felippe Vilas Boas
         </h1>
         <h2 className="text-lg text-black dark:text-gray-400">
@@ -33,7 +33,7 @@ const PresentationCard = () => {
         alt="Foto de Perfil"
         height={150}
         width={150}
-        className="hidden sm:block rounded-xl"
+        className="hidden xs:block rounded-xl"
       />
     </Row>
   );
