f2e0c4b78cf0a61528cadccc84d142f1e251418f
fsvboas <fsvboas.dev@gmail.com>
2025-06-10 14:40:15 -0300

    refactor: update work-experience-card and work-experiences-section components with new data

--- app/components/work-experience-card.tsx
+++ app/components/work-experience-card.tsx
@@ -3,23 +3,28 @@ import Link from "next/link";
 import { WorkExperiencesType } from "../types/work-experiences-type";
 import { Column } from "./utils/column";
 import { Flex } from "./utils/flex";
+import { Show } from "./utils/show";
 
 interface WorkExperienceCardProps {
   workExperience: WorkExperiencesType;
 }
 
 const WorkExperienceCard = ({ workExperience }: WorkExperienceCardProps) => {
+  const companyLinkExists = workExperience.companyLink !== "";
+
   return (
     <Column className="justify-between border border-gray-300 dark:border-gray-600 p-4 rounded-xl bg-gray-200 dark:bg-[#202020] space-y-2 hover:border-gray-400 dark:hover:border-gray-400 duration-300">
       <Flex className="flex-col sm:flex-row sm:items-center justify-between">
         <h4 className="font-bold">
           <Link
             href={workExperience.companyLink}
-            target="_blank"
+            target={companyLinkExists ? "_blank" : ""}
             className="hover:underline flex flex-row items-center space-x-1"
           >
             <p>{workExperience.company}</p>
-            <LucideLink size={16} />
+            <Show when={companyLinkExists}>
+              <LucideLink size={16} />
+            </Show>
           </Link>
         </h4>
         <span className="text-sm text-gray-600 dark:text-gray-400">
--- app/components/work-experiences-section.tsx
+++ app/components/work-experiences-section.tsx
@@ -5,10 +5,18 @@ import WorkExperienceCard from "./work-experience-card";
 const WorkExperiencesSection = () => {
   const workExperiences: WorkExperiencesType[] = [
     {
+      company: "Freelance",
+      companyLink: "",
+      employmentDuration: "Agosto de 2024 - Dezembro de 2024",
+      jobTitle: "Desenvolvedor Front-end Web",
+      jobDescription:
+        "Como freelancer, fui responsável pela manutenção de um software já existente, buscando otimizar a interatividade do usuário, o desempenho e os mecanismos de busca (SEO) do produto.",
+    },
+    {
       company: "80 Lines",
       companyLink: "https://br.linkedin.com/company/80lines",
       employmentDuration: "Junho de 2022 - Dezembro de 2023",
-      jobTitle: "Desenvolvedor Front-end",
+      jobTitle: "Desenvolvedor Front-end Web & Mobile",
       jobDescription: (
         <>
           Entrei na 80 Lines como desenvolvedor front-end trainee, onde tive a
@@ -38,17 +46,8 @@ const WorkExperiencesSection = () => {
       companyLink: "https://www.linkedin.com/company/neoris/",
       employmentDuration: "Maio de 2021 - Novembro de 2021",
       jobTitle: "Desenvolvedor SAP",
-      jobDescription: (
-        <>
-          Iniciei minha carreira profissional na NEORIS, como desenvolvedor SAP
-          trainee. Esta foi uma importante porta de entrada para o mercado de
-          trabalho, permitindo-me adquirir os conhecimentos fundamentais sobre o
-          funcionamento do setor de tecnologia. Durante minha trajetória na
-          empresa, trabalhei tanto com desenvolvimento do front-end quanto o do
-          back-end das aplicações, o que me proporcionou uma visão ampla e
-          aprofundada de todas as etapas de criação e manutenção de sistemas.
-        </>
-      ),
+      jobDescription:
+        "Iniciei minha carreira profissional na NEORIS, como desenvolvedor SAP trainee. Esta foi uma importante porta de entrada para o mercado de trabalho, permitindo-me adquirir os conhecimentos fundamentais sobre o funcionamento do setor de tecnologia. Durante minha trajetória na empresa, trabalhei tanto com desenvolvimento do front-end quanto o do back-end das aplicações, o que me proporcionou uma visão ampla e aprofundada de todas as etapas de criação e manutenção de sistemas.",
     },
   ];
 
