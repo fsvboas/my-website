692d53bfb3317f20607691052e58010b28f00e18
fsvboas <fsvboas.dev@gmail.com>
2025-07-19 14:21:20 -0300

    feat: create fade-in-stagger animation component

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,22 @@
+import FadeInStagger from "./components/animations/fade-in-stagger";
 import EducationSection from "./components/education-section";
 import PresentationCard from "./components/presentation-card";
 import SkillsSection from "./components/skills-section";
 import SummarySection from "./components/summary-section";
 import SwitchThemeButton from "./components/switch-theme-button";
-import { Column } from "./components/utils/column";
 import WorkExperiencesSection from "./components/work-experiences-section";
 
 export default function Home() {
   return (
-    <div className="flex w-full min-h-screen justify-center bg-[#f4f2ee] dark:bg-[#101010] p-6 md:p-16">
-      <Column className="w-full max-w-screen-md space-y-10">
-        <SwitchThemeButton />
+    <div className="flex w-full min-h-screen justify-center bg-[#f4f2ee] dark:bg-[#101010] p-6 md:p-16 relative">
+      <SwitchThemeButton />
+      <FadeInStagger className="flex flex-col w-full max-w-screen-md space-y-10 mt-10">
         <PresentationCard />
         <SummarySection />
         <WorkExperiencesSection />
         <EducationSection />
         <SkillsSection />
-      </Column>
+      </FadeInStagger>
     </div>
   );
 }
