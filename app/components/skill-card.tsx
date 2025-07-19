e96f1518da90741b5048a8abffff36efc184c2ad
fsvboas <fsvboas.dev@gmail.com>
2025-07-19 12:52:48 -0300

    refactor: minor improvements to skills section

--- app/components/skill-card.tsx
+++ app/components/skill-card.tsx
@@ -1,19 +1,21 @@
 import Image from "next/image";
-import { Column } from "./utils/column";
+import Link from "next/link";
+import { SkillsType } from "../types/skills-type";
 
 interface SkillCardProps {
-  skill: {
-    src: string;
-    name: string;
-  };
+  skill: SkillsType;
 }
 
 const SkillCard = ({ skill }: SkillCardProps) => {
   return (
-    <Column className="h-[140px] xs:w-[140px] items-center justify-between border border-gray-300 dark:border-gray-600 dark:bg-[#202020] p-5 rounded-xl hover:scale-105 duration-300">
+    <Link
+      className="flex flex-col h-[140px] xs:w-[140px] items-center justify-between border border-gray-300 dark:border-gray-600 dark:bg-[#202020] p-5 rounded-xl hover:scale-105 duration-300 hover:border-gray-400 dark:hover:border-gray-400"
+      href={skill.link}
+      target="_blank"
+    >
       <Image height={80} width={80} src={skill.src} alt={skill.name} />
       <span className="text-xs">{skill.name}</span>
-    </Column>
+    </Link>
   );
 };
 
