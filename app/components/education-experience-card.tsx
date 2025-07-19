08f1f830a1ef52adc62fec15eaaaf5f9ee70e690
fsvboas <fsvboas.dev@gmail.com>
2025-07-19 13:13:10 -0300

    refactor: minor improvements to education-section code

---
+++ app/components/education-experience-card.tsx
@@ -0,0 +1,30 @@
+import { EducationExperiencesType } from "../types/education-experiences-type";
+import { Column } from "./utils/column";
+import { Flex } from "./utils/flex";
+
+interface EducationExperienceCardProps {
+  educationExperience: EducationExperiencesType;
+}
+
+const EducationExperienceCard = ({
+  educationExperience,
+}: EducationExperienceCardProps) => {
+  return (
+    <Column className="justify-between border border-gray-300 dark:border-gray-600 p-4 rounded-xl bg-gray-200 dark:bg-[#202020]  space-y-2 hover:border-gray-400 dark:hover:border-gray-400 duration-300">
+      <Flex className="flex-col sm:flex-row sm:items-center justify-between">
+        <h4 className="font-bold">{educationExperience.institution}</h4>
+        <span className="text-sm text-gray-600 dark:text-gray-400">
+          {educationExperience.courseDuration}
+        </span>
+      </Flex>
+      <p className="text-sm">{educationExperience.courseName}</p>
+      {educationExperience?.courseDescription && (
+        <p className="text-gray-600 dark:text-gray-400 text-justify text-sm">
+          {educationExperience?.courseDescription}
+        </p>
+      )}
+    </Column>
+  );
+};
+
+export default EducationExperienceCard;
